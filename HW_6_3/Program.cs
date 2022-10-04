72611325d2e01f0bd8e8be387e753d619733ca14
oleksii <alexbuttko@gmail.com>
2022-10-04 16:35:42 +0300

    hw6

---
+++ HW_6_3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace HW_6_3
+{
+    internal class Program
+    {
+        static ulong Factorial(uint n)
+        {
+                         
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return Factorial(n - 1) * n;
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter factorial n = ");
+            var n = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine($"Factorial of a number {n} is equal {Factorial(n)}");
+            Console.ReadLine();
+        }
+    }
+}
